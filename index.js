f5c3461e170cea16f9615869761ae7055c916c0f
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-03-08 15:06:17 +0530

    donr

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ require("./db/connect");
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIESECRET));
 app.enable('trust proxy')
+app.use((req,res,next)=>{
+    res.set("Access-Control-Allow-Origin","http://localhost:3000")
+    res.set("Access-Control-Allow-Headers","Content-type")
+    console.log(res.getHeader("Access-Control-Allow-Origin"))
+    next()
+})
 
 
 //routes merge point
