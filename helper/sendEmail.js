98f381ad57ad9a86338cb7d80bbf41e47fefd60a
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2022-06-14 16:14:38 +0530

    changed smtp google to hostinger

--- helper/sendEmail.js
+++ helper/sendEmail.js
@@ -7,7 +7,7 @@ const nodemailer=require("nodemailer");
 
 
     let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
+        host: "smtp.hostinger.com",
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
